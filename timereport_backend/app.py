d45a8d23915a5a13aee352d75408b010f01a2e8d
Kamjar Gerami <kami.gerami@gmail.com>
2018-12-31 15:25:56 +0100

    app.py: delete /timereport/ prefix endpoint route since this will be handled in the stage for api gw

--- timereport_backend/app.py
+++ timereport_backend/app.py
@@ -3,13 +3,14 @@ from chalice import Chalice
 from chalicelib.lib import dynamo
 
 """
-GET        /timereport/user/<user_id>
-GET        /timereport/user/<user_id>?startDate=YYYYMMDD&endDate=YYYYMMDD
-POST       /timereport/event
-GET        /timereport/event/?startDate=YYYYMMDD&endDate=YYYYMMDD
-GET        /timereport/event/<_id>
-PUT        /timereport/event/<_id>
-DELETE     /timereport/event/<_id>
+GET        /user/<user_id>
+GET        /user/<user_id>?startDate=YYYYMMDD&endDate=YYYYMMDD
+POST       /event
+GET        /event/?startDate=YYYYMMDD&endDate=YYYYMMDD
+GET        /event/<_id>
+PUT        /event/<_id>
+DELETE     /event/<_id>
+GET        /table-name  : returns name of dynamodb table (good for testing connection)
 
 {'user_id': 'U2FGC795G', 'user_name': 'kamger', 'reason': 'vab', 'event_date': datetime.datetime(2018, 12, 5, 0, 0), 'hours': '8'}
 {"user_id": "U2FGC795G", "user_name": "kamger", "reason": "vab", "event_date": "2018-12-03", "hours":8}
@@ -25,7 +26,7 @@ def test_name():
     """
     return dynamo.dynamoboto.table.name
 
-@app.route('/timereport/user/{user_id}', methods=['GET'])
+@app.route('/user/{user_id}', methods=['GET'])
 def get_user_by_id(user_id):
     if app.current_request.query_params:
         start_date = app.current_request.query_params.get('startDate')
@@ -34,19 +35,19 @@ def get_user_by_id(user_id):
     else:
         return dynamo.get_id(user_id)
 
-@app.route('/timereport/event', methods=['POST'])
+@app.route('/event', methods=['POST'])
 def create_event():
     dynamo.create_event(app.current_request.json_body)
     return app.current_request.json_body
 
-@app.route('/timereport/event/{_id}', methods=['GET'])
+@app.route('/event/{_id}', methods=['GET'])
 def get_event_by_id(_id):
     return {'event_id': _id}
 
-@app.route('/timereport/event/{_id}', methods=['PUT'])
+@app.route('/event/{_id}', methods=['PUT'])
 def put_event_by_id(_id):
     return app.current_request.json_body
 
-@app.route('/timereport/event/{_id}', methods=['DELETE'])
+@app.route('/event/{_id}', methods=['DELETE'])
 def delete_event_by_id(_id):
     return {'event_id': _id}
