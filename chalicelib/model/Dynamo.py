2c9e9b4b354df633efefc8a9f79ccf8c01bc8429
parberge <paer.berge@gmail.com>
2019-03-23 19:44:41 +0100

    Configure endpoint_url for boto3
    To make local development work we also need to tell boto3 the endpoint.

--- chalicelib/model/Dynamo.py
+++ chalicelib/model/Dynamo.py
@@ -27,7 +27,8 @@ class DynamoBoto(EventModel.Meta):
     """
     region = EventModel.Meta.region
     table_name = EventModel.Meta.table_name
-    dynamodb = boto3.resource("dynamodb", region_name=region)
+    host = EventModel.Meta.host
+    dynamodb = boto3.resource("dynamodb", region_name=region, endpoint_url=host)
     table = dynamodb.Table(table_name)
 
 
