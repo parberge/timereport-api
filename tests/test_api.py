4277c6d118bbb1db2f9c432cd8e35c651ea6c8f1
parberge <paer.berge@gmail.com>
2019-06-05 09:04:43 +0200

    Update tests
    With recent changes to the API the tests was broken.
    They are now updated with the correct endpoints.

--- tests/test_api.py
+++ tests/test_api.py
@@ -11,11 +11,11 @@ check_local_db = requests.get(f"{local_db}/shell/")
 if check_local_db.status_code != 200:
     raise Exception("Local DB check failed")
 
-check_local_api = requests.get(f"{local_api}/user/names")
+check_local_api = requests.get(f"{local_api}/table-name")
 if check_local_api.status_code != 200:
     raise Exception("Local API check failed")
 
 def test_get_id(caplog):
-    response = requests.get(f"{local_api}/user/fake")
+    response = requests.get(f"{local_api}/event/users/fake_user")
     assert response is not None
     assert response.text == '[]'
