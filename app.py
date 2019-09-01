35fb46130e1c17f083758e30f0030b82569a9594
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2019-09-01 16:21:24 +0200

    Major refactor
    - Using only pynamodb
    - Remove dependencies of boto
    
    Refactor and and new methods:
    Event:
    - get_all_user_ids
    - get_all_events
    - get_all_events_by_user_id
    - get_all_events_by_date
    - get_event_by_user_id_and_date
    - create_event
    - delete_event
    Locking:
    - create_lock
    - get_lock
    - delete_lock

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from chalice import Chalice
-from chalicelib.lib import dynamo_lock, dynamo_event
-from chalicelib.model import Dynamo
+from chalicelib.lib import db_functions
+from chalicelib.model.models import EventModel, LockModel
 import logging
 
 
@@ -9,11 +9,8 @@ app = Chalice(app_name='timereport_backend')
 app.debug = os.getenv('BACKEND_DEBUG', False)
 log = logging.getLogger(__name__)
 
-db_event = Dynamo.EventModel
-db_lock = Dynamo.LockModel
 
-# create tables
-for db in [db_event, db_lock]:
+for db in [EventModel, LockModel]:
     if not db.exists():
         db.create_table(read_capacity_units=1, write_capacity_units=1, wait=True)
 
@@ -23,44 +20,55 @@ def test_name():
     """
     :return: table name
     """
-    return {'name': [dynamo_event.dynamoboto.table.name, dynamo_lock.dynamoboto.table.name] }
+    return {'name': [EventModel.Meta.table_name, LockModel.Meta.table_name]}
+
+
+@app.route('/event', methods=['GET'], cors=True)
+def get_all_events():
+    return db_functions.get_all_events()
 
 
 @app.route('/event/users', methods=['GET'], cors=True)
 def get_user_ids():
-    return dynamo_event.get_user_ids()
+    return db_functions.get_all_user_ids()
 
 
 @app.route('/event/users/{user_id}', methods=['GET'], cors=True)
-def get_events_by_user_id(user_id):
-    start_date = None
-    end_date = None
-    if app.current_request.query_params:
-        log.debug(f"Got request params: {app.current_request.query_params}")
-        start_date = app.current_request.query_params.get('startDate')
-        end_date = app.current_request.query_params.get('endDate')
-
-    return dynamo_event.get_id(user_id=user_id, start_date=start_date, end_date=end_date)
+def get_all_events_by_user_id(user_id):
+    return db_functions.get_all_events_by_user_id(user_id=user_id)
 
 
-@app.route('/event/users/{user_id}', methods=['POST'], cors=True)
-def create_event(user_id):
-    dynamo_event.create_event(app.current_request.json_body, user_id)
-    return app.current_request.json_body
+@app.route('/event/users/{user_id}/{event_date}', methods=['GET'], cors=True)
+def delete_event(user_id, event_date):
+    return db_functions.get_event_by_user_id_and_date(user_id=user_id, event_date=event_date)
 
 
-@app.route('/event/users/{user_id}', methods=['DELETE'], cors=True)
-def delete_event_by_id(user_id):
-    if app.current_request.query_params:
-        start_date = app.current_request.query_params.get('date')
-        app.log.info(f'delete event backend: date is {start_date} and id is {user_id}')
-        return dynamo_event.delete_event(user_id, start_date)
+@app.route('/event/users/{user_id}/{event_date}', methods=['DELETE'], cors=True)
+def delete_event(user_id, event_date):
+    return db_functions.delete_event(user_id=user_id, event_date=event_date)
+
+
+@app.route('/event/date/{event_date}', methods=['GET'], cors=True)
+def get_events_by_date(event_date):
+    return db_functions.get_all_events_by_date(event_date=event_date)
+
+
+@app.route('/event', methods=['POST'], cors=True)
+def create_event():
+    return db_functions.create_event(app.current_request.json_body)
+
 
 @app.route('/lock/users/{user_id}/{event_date}', methods=['GET'], cors=True)
 def get_lock(user_id, event_date):
-    return dynamo_lock.get_lock(user_id=user_id, event_date=event_date)
+    return db_functions.get_lock(user_id=user_id, event_date=event_date)
+
 
 @app.route('/lock', methods=['POST'], cors=True)
 def create_lock():
-    dynamo_lock.create_lock(app.current_request.json_body)
+    db_functions.create_lock(app.current_request.json_body)
     return app.current_request.json_body
+
+
+@app.route('/lock/{user_id}/{event_date}', methods=['DELETE'], cors=True)
+def delete_lock(user_id, event_date):
+    return db_functions.delete_lock(user_id, event_date)
