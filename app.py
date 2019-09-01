13d16a9e769e24060ae193ef62192cf455b171fa
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2019-09-01 19:53:50 +0200

    Use plural names in RESTful resources

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ for db in [EventModel, LockModel]:
         db.create_table(read_capacity_units=1, write_capacity_units=1, wait=True)
 
 
-@app.route('/table-name', cors=True)
+@app.route('/table-names', cors=True)
 def test_name():
     """
     :return: table name
@@ -23,62 +23,62 @@ def test_name():
     return {'name': [EventModel.Meta.table_name, LockModel.Meta.table_name]}
 
 
-@app.route('/event', methods=['GET'], cors=True)
+@app.route('/events', methods=['GET'], cors=True)
 def get_all_events():
     return db_functions.get_all_events()
 
 
-@app.route('/event/users', methods=['GET'], cors=True)
+@app.route('/events/users', methods=['GET'], cors=True)
 def get_user_ids():
     return db_functions.get_all_user_ids()
 
 
-@app.route('/event/users/{user_id}', methods=['GET'], cors=True)
+@app.route('/events/users/{user_id}', methods=['GET'], cors=True)
 def get_all_events_by_user_id(user_id):
     return db_functions.get_all_events_by_user_id(user_id=user_id)
 
 
-@app.route('/event/users/{user_id}/{event_date}', methods=['GET'], cors=True)
+@app.route('/events/users/{user_id}/{event_date}', methods=['GET'], cors=True)
 def delete_event(user_id, event_date):
     return db_functions.get_event_by_user_id_and_date(user_id=user_id, event_date=event_date)
 
 
-@app.route('/event/users/{user_id}/{event_date}', methods=['DELETE'], cors=True)
+@app.route('/events/users/{user_id}/{event_date}', methods=['DELETE'], cors=True)
 def delete_event(user_id, event_date):
     return db_functions.delete_event(user_id=user_id, event_date=event_date)
 
 
-@app.route('/event/date/{event_date}', methods=['GET'], cors=True)
+@app.route('/events/dates/{event_date}', methods=['GET'], cors=True)
 def get_events_by_date(event_date):
     return db_functions.get_all_events_by_date(event_date=event_date)
 
 
-@app.route('/event', methods=['POST'], cors=True)
+@app.route('/events', methods=['POST'], cors=True)
 def create_event():
     return db_functions.create_event(app.current_request.json_body)
 
 
-@app.route('/lock', methods=['GET'], cors=True)
+@app.route('/locks', methods=['GET'], cors=True)
 def get_all_locks():
     return db_functions.get_all_locks()
 
 
-@app.route('/lock/users/{user_id}/{event_date}', methods=['GET'], cors=True)
+@app.route('/locks/users/{user_id}/{event_date}', methods=['GET'], cors=True)
 def get_lock(user_id, event_date):
     return db_functions.get_lock(user_id=user_id, event_date=event_date)
 
 
-@app.route('/lock', methods=['POST'], cors=True)
+@app.route('/locks', methods=['POST'], cors=True)
 def create_lock():
     db_functions.create_lock(app.current_request.json_body)
     return app.current_request.json_body
 
 
-@app.route('/lock/users/{user_id}/{event_date}', methods=['DELETE'], cors=True)
+@app.route('/locks/users/{user_id}/{event_date}', methods=['DELETE'], cors=True)
 def delete_lock(user_id, event_date):
     return db_functions.delete_lock(user_id, event_date)
 
 
-@app.route('/lock/date/{event_date}', methods=['DELETE'], cors=True)
+@app.route('/locks/dates/{event_date}', methods=['DELETE'], cors=True)
 def delete_all_locks_by_date(event_date):
     return db_functions.delete_all_locks_by_date(event_date)
