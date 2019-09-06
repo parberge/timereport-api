4706535b546c3dca131411e9d989b837fdd6f630
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2019-09-06 22:36:56 +0200

    Add more tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,6 +2,21 @@ import requests
 
 local_api = 'http://localhost:8010'
 local_db = 'http://localhost:8000'
+testuser = "testuser"
+testmonth = "2019-03"
+testdate = "2019-03-21"
+event_data = {
+    "reason": "sick",
+    "hours": "8",
+    "user_name": "Test Mctest",
+    "user_id": f"{testuser}",
+    "event_date": f"{testdate}"
+}
+lock_data = {
+    "user_id": f"{testuser}",
+    "event_date": f"{testmonth}"
+}
+
 
 # sanity check to see that we have some certainty that this test will execute
 # on local environment
@@ -14,22 +29,110 @@ if check_local_api.status_code != 200:
     raise Exception("Local API check failed")
 
 
+def create_event():
+    r = requests.post(f"{local_api}/events", json=event_data)
+    return r
+
+
+def create_lock():
+    r = requests.post(f"{local_api}/locks", json=lock_data)
+    return r
+
+
 def test_create_event():
-    data = {
-        "reason": "sick",
-        "hours": "8",
-        "user_name": "Mr User",
-        "user_id": "user101",
-        "event_date": "2019-03-21"
-    }
-    r = requests.post(f"{local_api}/events", json=data)
+    r = create_event()
     assert r.status_code == 200
 
 
 def test_create_lock():
-    data = {
-        "user_id": "user101",
-        "event_date": "2019-03"
-    }
-    r = requests.post(f"{local_api}/events", json=data)
+    r = create_lock()
+    assert r.status_code == 200
+
+
+def test_list_users():
+    r = requests.get(f"{local_api}/users")
+    assert r.status_code == 200
+
+
+def test_get_user():
+    create_event()
+    r = requests.get(f"{local_api}/users/{testuser}")
+    assert r.status_code == 200
+
+
+def test_list_events_by_user_id():
+    create_event()
+    r = requests.get(f"{local_api}/users/{testuser}/events")
+    assert r.status_code == 200
+
+
+def test_delete_all_events_by_user_id():
+    create_event()
+    r = requests.delete(f"{local_api}/users/{testuser}/events")
+    assert r.status_code == 200
+
+
+def test_list_locks_by_user_id():
+    create_lock()
+    r = requests.get(f"{local_api}/users/{testuser}/locks")
+    assert r.status_code == 200
+
+
+def test_delete_all_locks_by_user_id():
+    create_lock()
+    r = requests.delete(f"{local_api}/users/{testuser}/locks")
+    assert r.status_code == 200
+
+
+def test_delete_lock_by_user_id_and_date():
+    create_lock()
+    r = requests.delete(f"{local_api}/users/{testuser}/locks/{testmonth}")
+    assert r.status_code == 200
+
+
+def test_get_event_by_user_id_and_date():
+    create_event()
+    r = requests.get(f"{local_api}/users/{testuser}/events/")
+    assert r.status_code == 200
+
+
+def test_delete_event_by_user_id_and_date():
+    create_event()
+    r = requests.delete(f"{local_api}/users/{testuser}/events/{testdate}")
+    assert r.status_code == 200
+
+
+def test_list_all_events():
+    create_event()
+    r = requests.get(f"{local_api}/events")
+    assert r.status_code == 200
+
+
+def test_list_all_events_by_date():
+    create_event()
+    r = requests.get(f"{local_api}/events/dates/{testdate}")
+    assert r.status_code == 200
+
+
+def test_delete_all_events_by_date():
+    create_event()
+    r = requests.delete(f"{local_api}/events/dates/{testdate}")
+    assert r.status_code == 200
+
+
+def test_list_all_locks():
+    create_lock()
+    r = requests.get(f"{local_api}/locks")
+    assert r.status_code == 200
+
+
+def test_list_all_locks_by_date():
+    create_lock()
+    r = requests.get(f"{local_api}/locks/dates/{testmonth}")
+    assert r.status_code == 200
+
+
+def test_delete_all_locks_by_date():
+    create_lock()
+    r = requests.delete(f"{local_api}/locks/dates/{testmonth}")
     assert r.status_code == 200
